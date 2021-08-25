f1f41905d02a338e5a438a9bd7b4073c1f95f982
Nate Lu <nate@970px.com>
2021-08-26 00:00:36 +0800

    ⬆️ Up Swiper 7

--- src/week8/js/main.js
+++ src/week8/js/main.js
@@ -1,7 +1,5 @@
 import 'bootstrap';
-import Swiper from 'swiper';
-import SwiperCore, { Navigation } from 'swiper/core';
-SwiperCore.use(Navigation);
+import Swiper, { Navigation, Grid } from 'swiper';
 
 // 漢堡點擊動畫
 const hamburger = document.querySelector(".hamburger");
@@ -10,26 +8,31 @@ hamburger.addEventListener("click", function() {
 });
 
 new Swiper(".columnSwiper", {
+  modules: [Navigation, Grid],
   slidesPerView: 1,
-  slidesPerColumn: 3,
   spaceBetween: 8,
-  slidesPerColumnFill: 'row',
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  grid: {
+    rows: 3,
+    fill: 'row'
+  },
   breakpoints: {
     768: {
-      slidesOffsetBefore: 16,
-      slidesOffsetAfter: 16,
       slidesPerView: 2.1,
-      slidesPerColumn: 2,
       spaceBetween: 16,
+      grid: {
+        rows: 2,
+      },
     },
     1200: {
       slidesPerView: 3,
-      slidesPerColumn: 2,
       spaceBetween: 30,
+      grid: {
+        rows: 2,
+      },
     }
   }
 });
